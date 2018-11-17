e893e9da0e05ee67f4eaf0111e99ceb09e80f474
Hemmy <hema.kappa55@gmail.com>
2018-11-17 05:56:56 +0200

    Updates

---
+++ precache-manifest.e71809b51ee0a506fa0246d9de1e0d23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b422b9bd22fd7f12e4482b7838467c4b",
+    "url": "/hemmy-form/static/media/wink.b422b9bd.png"
+  },
+  {
+    "revision": "3dbc5daafe5e16c88945a7c74fd0cb70",
+    "url": "/hemmy-form/static/media/girl1.3dbc5daa.png"
+  },
+  {
+    "revision": "276040b13a144c81266c",
+    "url": "/hemmy-form/static/js/runtime~main.276040b1.js"
+  },
+  {
+    "revision": "97a65b93219f37db8dfd",
+    "url": "/hemmy-form/static/js/main.97a65b93.chunk.js"
+  },
+  {
+    "revision": "75251cb8a6e2a79cd339",
+    "url": "/hemmy-form/static/js/1.75251cb8.chunk.js"
+  },
+  {
+    "revision": "97a65b93219f37db8dfd",
+    "url": "/hemmy-form/static/css/main.9b73a922.chunk.css"
+  },
+  {
+    "revision": "75251cb8a6e2a79cd339",
+    "url": "/hemmy-form/static/css/1.18e2ab43.chunk.css"
+  },
+  {
+    "revision": "5e511cd88c67a1f5201a74e0bb3ccaf8",
+    "url": "/hemmy-form/index.html"
+  }
+];
